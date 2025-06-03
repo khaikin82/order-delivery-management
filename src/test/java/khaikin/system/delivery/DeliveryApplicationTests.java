4a70b63e965530402a2d91000c53f7d23cadf2d2
khaihd <hoangdangkhai.dev@gmail.com>
2025-06-03 20:41:52 +0700

    first commit

---
+++ src/test/java/khaikin/system/delivery/DeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package khaikin.system.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
